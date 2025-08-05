49c520ed7af6af13b1955a7377482a9c03e14fb4
Samiullah Shaikh <samiullah21january@gmail.com>
2025-08-05 05:46:05 -0700

    Added maps in footer

--- app/layout.js
+++ app/layout.js
@@ -16,19 +16,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={``}>
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        <Toaster richColors/>
+      <html lang="en">
+        <body className={``}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
 
-        <footer className="bg-blue-50 py-15">
-          <div className="container mx-auto py-4 text-center text-gray-600">
-            <p className="">© 2023 AICar Marketplace. All rights reserved.</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="py-16 dotted-background ">
+            <div className="mx-10 border border-gray-400 px-1 py-1 ">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.0624658293486!2d67.13748587393775!3d24.929941842476442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb338f5c31a8d97%3A0xd09821dc9c7cb62f!2sHascol%20Petrol%20Pump-jauhar!5e0!3m2!1sen!2s!4v1754396923433!5m2!1sen!2s"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </div>
+
+            <div className="container mx-auto py-4 text-center text-gray-200">
+              <p className="">© 2023 AICar Marketplace. All rights reserved.</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
